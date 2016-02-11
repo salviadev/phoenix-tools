a7d979fb547fd43a0e5be9fa9bb9c11edb1bba21
misto <mihaistoie@gmail.com>
2016-02-11 10:23:29 +0100

    #

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,22 @@ var merge = require('merge2');
 var ts = require('gulp-typescript');
 var dts = require('dts-bundle');
 var runSequence = require('run-sequence');
+var shell = require('gulp-shell');
+
+
+gulp.task('upgrade-phoenix-cli', shell.task(['npm install']));
+
+gulp.task('remove-phoenix-libs', function () {
+    return del([
+        'node_modules/phoenix-*'
+    ]);
+});
+
+gulp.task('upgrade', function (done) {
+    runSequence('remove-phoenix-libs', 'upgrade-phoenix-cli', done);
+});
+
+
 
 
 gulp.task('clean', function () {
