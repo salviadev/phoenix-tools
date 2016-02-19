7bfc383bf90b43d6baca292d4c19624e38fb32bf
misto <mihaistoie@gmail.com>
2016-02-19 20:57:19 +0100

    #

--- lib/definitions/lib/utils.d.ts
+++ lib/definitions/lib/utils.d.ts
@@ -1,4 +1,5 @@
 export declare var utils: {
     clone: (src: any, recursive: boolean) => any;
     value: (value: any, path: string) => any;
+    setValue: (obj: any, path: string, value: any) => void;
 };
--- lib/definitions/phoenix-utils.d.ts
+++ lib/definitions/phoenix-utils.d.ts
@@ -36,6 +36,7 @@ declare module 'phoenix-utils/lib/utils' {
     export var utils: {
         clone: (src: any, recursive: boolean) => any;
         value: (value: any, path: string) => any;
+        setValue: (obj: any, path: string, value: any) => void;
     };
 }
 
