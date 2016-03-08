ad5ebc5bb5c0e9949d3874166fa252fb7e47b599
misto <mihaistoie@gmail.com>
2016-03-08 13:19:37 +0100

    multitenant

--- src/lib/multitenant.ts
+++ src/lib/multitenant.ts
@@ -1,35 +1,39 @@
+
 "use strict";
-exports.Multitenant = {
+export var Multitenant = {
     DB: 'db',
     SCHEMA: 'schema',
     SHARE: 'share'
-};
-function _tenant2prefix(tenantId) {
+}
+
+function _tenant2prefix(tenantId: number): string {
     const pad = '00000000000000000000000';
     const tp = 5;
     let ts = tenantId + '';
     return pad.substr(0, tp - ts.length) + ts;
 }
-function schemaPrefix(tenantId, driver) {
+export function schemaPrefix(tenantId: number, driver: string): string {
     // driver = mongodb/mssql/ora
     return 'c' + _tenant2prefix(tenantId) + '.';
 }
-exports.schemaPrefix = schemaPrefix;
-function databaseName(tenantId, dbName, driver) {
+
+export function databaseName(tenantId: number, dbName: string, driver: string): string {
     // driver = mongodb/mssql/ora
     return 'c' + _tenant2prefix(tenantId) + '_' + dbName;
 }
-exports.databaseName = databaseName;
-function collectionName(tenantId, collectionName, driver) {
+
+
+export function collectionName(tenantId: number, collectionName: string, driver: string): string {
     // driver = mongodb/mssql/ora
     return schemaPrefix(tenantId, driver) + collectionName;
 }
-exports.collectionName = collectionName;
-exports.multitenant = {
+
+
+export var multitenant = {
     DB: 'db',
     SCHEMA: 'schema',
     SHARE: 'share',
     schemaPrefix: schemaPrefix,
     databaseName: databaseName,
     collectionName: collectionName
-};
+}
